715849b633162d9bd72dff9c99f537b256d4fbeb
Mateusz <kapuscinski.mateusz01@gmail.com>
2025-05-10 16:38:52 +0200

    The Legacy Way of Connecting Components to Redux

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -11,11 +11,9 @@ function AccountOperations() {
   const [currency, setCurrency] = useState("USD");
 
   const dispatch = useDispatch();
-  const {
-    loan: currentLoan,
-    loanPurpose: currentLoanPurpose,
-    balance,
-  } = useSelector((store) => store.account);
+  const { loan: currentLoan, loanPurpose: currentLoanPurpose } = useSelector(
+    (store) => store.account
+  );
 
   function handleDeposit() {
     if (!depositAmount) return;
