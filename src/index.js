66fa4e6842d4631714efe1245802a6c654a57f51
Mateusz <kapuscinski.mateusz01@gmail.com>
2025-05-10 15:07:37 +0200

    Professional Redux File Structure: State Slices

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import "./store";
+import store from "./store";
+store.dispatch({ type: "account/deposit", payload: 1000 });
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
